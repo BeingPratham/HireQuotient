aa04027ad9f2917c7496feee2bc6785989b6ddda
BeingPratham <upadhyayprathamm215@gmail.com>
2023-12-02 21:38:30 +0530

    port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import fetch from "node-fetch";
 import express from "express";
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
